dd54df0891174c4dd8898f48fbeae9c4fec0cbfb
Toni <tonirosinol@hotmail.com>
2019-09-21 19:08:34 -0400

    First iter

---
+++ README.md
@@ -0,0 +1,54 @@
+# ply_ros_publisher
+
+ROS node for ply_ros_publisher
+
+# 1. Installation
+
+## A. Prerequisities
+
+- [ROS](https://www.ros.org/install/)
+
+## B. Installation
+
+```bash
+# Setup catkin workspace
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/
+catkin init
+
+# Add workspace to bashrc.
+echo 'source ~/catkin_ws/devel/setup.bash' >> ~/.bashrc
+
+# Clone repo
+cd ~/catkin_ws/src
+git clone https://github.com/ToniRV/ply_ros_publisher.git
+
+# Install dependencies from rosinstall file using wstool
+wstool init
+wstool merge ply_ros_publisher/install/ply_ros_publisher.rosinstall
+wstool update
+```
+
+Finally, compile:
+
+```bash
+# Compile code
+catkin build
+
+# Refresh workspace
+source ~/.bashrc
+```
+
+# 2. Usage
+
+## rosrun
+```
+rosrun ply_ros_publisher ply_ros_publisher
+```
+
+## roslaunch
+```
+roslaunch ply_ros_publisher ply_ros_publisher
+```
+
+## rosservice
---
+++ src/ply_ros_publisher.cpp
@@ -0,0 +1,40 @@
+#include <ros/ros.h>
+
+#include "ply_ros_publisher/ply_ros_publisher.h"
+
+#include <pcl/io/ply_io.h>
+#include <pcl/point_types.h>
+#include <pcl/PolygonMesh.h>
+#include <pcl_msgs/PolygonMesh.h>
+#include <pcl_conversions/pcl_conversions.h>
+
+int main(int argc, char *argv[]) {
+  ros::init(argc, argv, "ply_ros_publisher");
+
+  ros::NodeHandle nh_private_ ("~");
+
+  std::string ply_filepath;
+  if (!nh_private_.getParam("ply_filepath", ply_filepath)) {
+    ROS_FATAL("No PLY filepath provided for! Set the \"ply_filepath\" param.");
+  }
+  std::string frame_id;
+  if (!nh_private_.getParam("frame_id", frame_id)) {
+    ROS_FATAL("No PLY filepath provided for! Set the \"frame_id\" param.");
+  }
+
+  pcl::PolygonMesh ply_mesh;
+  pcl::PLYReader ply_reader;
+  if (ply_reader.read(ply_filepath, ply_mesh), 0) {
+    ROS_FATAL("Could not load PLY file.");
+  }
+
+  pcl_msgs::PolygonMesh pcl_mesh_msg;
+  pcl_conversions::fromPCL(ply_mesh, pcl_mesh_msg);
+
+  ros::Publisher ply_pub_ =
+      nh_private_.advertise<pcl_msgs::PolygonMesh>("ply_mesh", 1, true);
+  //  pcl_mesh_msg.header.frame_id = frame_id;
+  ply_pub_.publish(pcl_mesh_msg);
+
+  return EXIT_SUCCESS;
+}
